81451408fc61059723c4a6944522983cd3912f5b
Thomas Lee <thomas.lee999@gmail.com>
2020-12-10 09:35:08 +0000

    Fix off by one error in validation

--- Sources/xcodeproj-modify/XcodeprojModify.swift
+++ Sources/xcodeproj-modify/XcodeprojModify.swift
@@ -1,15 +1,18 @@
 import Foundation
 import PathKit
-import xcodeproj
+import XcodeProj
 
 struct XcodeprojModify {
     enum Error: LocalizedError {
         case unknownTarget(String)
+        case invalidPosition(Int)
         
         var errorDescription: String? {
             switch self {
             case .unknownTarget(let string):
                 return "Couldn't find target named \(string)"
+            case .invalidPosition(let int):
+                return "Unable to place build script at position \(int)"
             }
         }
     }
@@ -39,12 +42,12 @@ struct XcodeprojModify {
     
     private func runCommand(_ command: Command, with arguments: Arguments) throws {
         switch command {
-        case .addRunScriptPhase(let target, let contents):
-            try addRunScriptPhase(target: target, contents: contents, xcodeprojPath: arguments.xcodeprojPath)
+        case .addRunScriptPhase(let target, let position, let name, let contents):
+            try addRunScriptPhase(target: target, position: position, name: name, contents: contents, xcodeprojPath: arguments.xcodeprojPath)
         }
     }
     
-    private func addRunScriptPhase(target: String, contents: String, xcodeprojPath: String) throws {
+    private func addRunScriptPhase(target: String, position: Int, name: String, contents: String, xcodeprojPath: String) throws {
         let path = Path(xcodeprojPath)
         let xcodeproj = try XcodeProj(path: path)
         let targets = xcodeproj.pbxproj.targets(named: target)
@@ -52,10 +55,29 @@ struct XcodeprojModify {
             throw Error.unknownTarget(target)
         }
 
-        let phase = PBXShellScriptBuildPhase(shellScript: contents)
+        let phase = PBXShellScriptBuildPhase(name: name, shellScript: contents)
         for target in targets {
-            target.buildPhases = target.buildPhases + [phase]
+            var buildPhases = target.buildPhases.filter {
+                ($0 as? PBXShellScriptBuildPhase)?.name != phase.name
+            }
+
+            if position < 0 {
+                buildPhases.append(phase)
+            } else {
+                guard position <= buildPhases.count else {
+                    throw Error.invalidPosition(position)
+                }
+                buildPhases.insert(phase, at: position)
+            }
+            target.buildPhases = buildPhases
+            print("Added build phase")
         }
-        try xcodeproj.write(path: path)
+        xcodeproj.pbxproj.add(object: phase)
+        try xcodeproj.writePBXProj(
+            path: path,
+            override: true,
+            outputSettings: PBXOutputSettings()
+        )
+        print("Successfully wrote data")
     }
 }
